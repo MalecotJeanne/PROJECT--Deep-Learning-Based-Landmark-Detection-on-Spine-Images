fixed backpropagation

models/hrnet.py
@@ -28,7 +28,8 @@ class HRNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64, momentum=0)
         self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(64, momentum=0)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
 
         # using bottleneck implementation from MONAI -> the expension  is always 4 in this class.
         self.bottleneck1 = SEResNetBottleneck(
@@ -209,21 +210,17 @@ class HRNet(nn.Module):
                 x_list.append(y_list[i])
         y_list = self.stage4(x_list)
 
-        # x = self.final_layer(y_list[0])
-        # return x
-
         x = self.final_layer(y_list[0])
-        batch_size, num_landmarks, height, width = x.shape  # [B, C, H, W]
+        
+        #convert into a probability map, flatten (last two dims) it and apply softmax
+        H,W = x.size()[-2:]
+        x = x.view(x.size(0), x.size(1), -1)
+        x = self.softmax(x)
+        x = x.view(x.size(0), x.size(1), H, W)
 
-        x_flat = x.view(batch_size, num_landmarks, -1)  # [B, C, H*W]
-        max_indices = x_flat.argmax(dim=2)
+        return x
 
-        y_coords = max_indices // width
-        x_coords = max_indices % width
-
-        landmarks = torch.stack((x_coords, y_coords), dim=2).float()
-
-        return landmarks
+        
 
 
 class HighResModule(nn.Module):
@@ -246,7 +243,7 @@ class HighResModule(nn.Module):
         self.out_channels = out_channels
         self.spatial_dims = spatial_dims
         self.multi_scale_output = multi_scale_output
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
 
         self.branches = nn.ModuleList()
         self.fuse_layers = nn.ModuleList()

utils.py
@@ -1,5 +1,5 @@
 """
-docstring
+TODO: docstring
 """
 
 import numpy as np
@@ -7,6 +7,7 @@ import os
 import glob
 import yaml
 import torch
+import cv2
 
 from scipy.io import loadmat
 
@@ -71,3 +72,23 @@ def load_config(config_dir):
     with open(config_dir, "r", encoding="utf-8") as file:
         config = yaml.safe_load(file)
     return config
+
+
+def save_images(images, save_dir, basename="image"):
+    """
+    Save the images in the given directory
+    """
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    n_images = len(images)
+    images_copy = images.clone()
+    for i in range(n_images):
+        image = images_copy[i].detach().cpu().numpy()
+        # map the values to [0, 255]
+        image = image - np.min(image)
+        image = (image / np.max(image)) * 255
+        save_path = os.path.join(save_dir, f"{basename}_{i}.jpg")
+        # save the image with cmap jet
+        cv2.imwrite(
+            save_path, cv2.applyColorMap(image.astype(np.uint8), cv2.COLORMAP_JET)
+        )